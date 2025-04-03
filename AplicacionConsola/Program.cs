8b4451296f8c0aa3eb6a865e5600638a5bff4cf4
brandoncahuecc <brandoncahuecc@outlook.com>
2025-04-03 08:03:37 -0600

    Plantilla de Proyectos

---
+++ AplicacionConsola/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Este es comentario
+ * de varias lineas
+ * en c# */
+
+//Este es un comentario
+int miInt = 10; //Asignando mi numero
+Console.WriteLine(miInt);
+Console.WriteLine(Environment.NewLine);
+
+long miLong = 20;
+Console.WriteLine(miLong);
+Console.WriteLine(Environment.NewLine);
+
+float miFloat = 20.39483F;
+Console.WriteLine(miFloat);
+Console.WriteLine(Environment.NewLine);
+
+double miDouble = 20.34899;
+Console.WriteLine(miDouble);
+Console.WriteLine(Environment.NewLine);
+
+decimal miDecimal = 10.38473M;
+Console.WriteLine(miDecimal);
+Console.WriteLine(Environment.NewLine);
+
+char miChar = '3';
+Console.WriteLine(miChar);
+Console.WriteLine(Environment.NewLine);
+
+bool miBool = true;
+Console.WriteLine(miBool);
+Console.WriteLine(Environment.NewLine);
+
+DateTime miDateTime = new DateTime(2004, 10, 23);
+Console.WriteLine(miDateTime);
+Console.WriteLine(Environment.NewLine);
+
+string miString = "Hola mundo";
+Console.WriteLine(miString);
+Console.WriteLine(Environment.NewLine);
